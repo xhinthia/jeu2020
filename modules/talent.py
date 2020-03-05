2c0fd692969cd4ea620b1fb0a36604489e78b6c8
Support Technique <support@itm.fr>
2020-03-05 17:28:36 +0100

    fonction talent presque terminée !

--- modules/talent.py
+++ modules/talent.py
@@ -45,30 +45,129 @@ else:
 
 """
 
-def affichage_talent(clean,fichier):
-    liste = []
-    with open(fichier,"r") as f_read:
-        for line in f_read:
-            line=line.strip()
-            liste.append(line)
-    f_read.close()
-    if clean==1:
-		os.system('cls' if os.name == 'nt' else 'clear')
-	print ("**********************************")
-	print ("")
-	print ("	"+str(liste[0]))
-	print ("")
-	print ("**********************************")
-	print ("")
-	print ("<><><><><><><><><><><><><><><><><>")
-	print ("")
-	j=1
-	for i in liste:
-            if i==0:
-                pass
+def talent(fichier1,fichier2,point_aptitude):
+    a=1
+    while a==1:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        liste1 = []
+        with open(fichier1,"r") as f_read:
+            for line in f_read:
+                line=line.strip()
+                liste1.append(line)
+        f_read.close()
+        print ("**********************************")
+        print ("")
+        print ("	"+str(liste1[1]))
+        print ("")
+        print ("**********************************")
+        print ("")
+        print ("<><><><><><><><><><><><><><><><><>")
+        print ("")
+        x=0
+        y=1
+        for x in range (2,18,2):
+            if liste1[x]!=0:
+                print ("Palier "+str(y)+" : "+str(liste1[x+1]))
+                y+=1
             else:
-                print ("Palier "+str(j)+" : "+str(i))
-            j+=1
-	print ("")
-	print ("<><><><><><><><><><><><><><><><><>")
+                pass
+        print ("")
+        print ("<><><><><><><><><><><><><><><><><>")
+
+        print ("")
+        print ("")
+
+        liste2 = []
+        with open(fichier2,"r") as f_read:
+            for line in f_read:
+                line=line.strip()
+                liste2.append(line)
+        f_read.close()
+        print ("**********************************")
+        print ("")
+        print ("    "+str(liste2[1]))
+        print ("")
+        print ("**********************************")
+        print ("")
+        print ("<><><><><><><><><><><><><><><><><>")
+        print ("")
+        i=0
+        j=1
+        for i in range (2,18,2):
+            if liste2[i]!=0:
+                print ("Palier "+str(j)+" : "+str(liste2[i+1]))
+                j+=1
+            else:
+                pass
+        print ("")
+        print ("<><><><><><><><><><><><><><><><><>")
+
+        print (" ")
+        print (" ")
+
+        if j==10:
+            print ("Vos choix ont tous été faits ici !")
+            input("Appuyez sur une touche pour sortir")
+            a=0
+            pass
+        print (str(point_aptitude)+" points à dépenser")
+        if point_aptitude==0:
+            input("Appuyez sur une touche pour sortir")
+            a=0
+            pass
+        else:
+            choix=input("Voulez-vous utilisez vos points ? (1 - Oui, 2 - Non) : ")
+            print ("")
+            b=1
+            while b==1:
+                if str(choix)=="2":
+                    input("Appuyez sur une touche pour sortir")
+                    a=0
+                    b=0
+                    pass
+                elif str(choix)=="1":
+                    if liste2[1]=="VIVACITE":
+
+                    elif liste2[1]=="PUISSANCE":
+
+                    elif liste2[1]=="RESISTANCE":
+
+                    elif liste2[1]=="INSAISISSABLE":
+
+                    elif liste2[1]=="ELEMENTAIRE":
+
+                    elif liste2[1]=="SANGUINAIRE":
+
+
+
+        p=x//2
+    prop=x+1
+    with open("trait_selection.txt","r") as f_read:
+        for line in range (1):
+            trait = f_read.readline()
+    f_read.close()
+    print ("")
+    print ("")
+    if point_aptitude==0:
+        print ("Vous n'avez plus de points à dépenser")
+        input("Appuyez sur une touche pour sortir")
+        a=0
+        pass
+    else:
+        print ("Tu as "+str(point_aptitude)+" à dépenser")
+        b=1
+        while b==1:
+            choix=input("Voulez-vous les utiliser ? (1 - Oui, 2- Non) : ")
+            print ("")
+            if str(choix)=="1":
+                if str(trait)=="VIVACITE"
+                c=1
+                while c==1:
+                    
+
+            elif str(choix)=="2":
+                input("Appuyez sur une touche pour sortir")
+                a=0
+                b=0
+                pass
 
